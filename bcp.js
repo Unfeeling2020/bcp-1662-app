3e563ac7b73d8b912fd4228698e4386d4978a14f
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 10:01:07 -0500

    Update bcp.js

--- bcp.js
+++ bcp.js
@@ -9,6 +9,12 @@ const bcp = {
     },
     litany: {
         title: "<h3>The Litany</h3>",
-        text: "<p>O God the Father, of heaven : have mercy upon us miserable sinners.<br><i>O God the Father, of heaven : have mercy upon us miserable sinners.</i></p><p>O God the Son, Redeemer of the world : have mercy upon us miserable sinners...</p>"
+        text: "<p>O God the Father, of heaven : have mercy upon us miserable sinners.<br><i>O God the Father, of heaven : have mercy upon us miserable sinners.</i></p>"
+    },
+    psalter: {
+        day9: {
+            morning: "<h3>The Psalms</h3><p><b>Psalm 44.</b> <i>Deus, auribus.</i><br>We have heard with our ears, O God, our fathers have told us : what thou hast done in their time of old...</p><p><b>Psalm 45.</b> <i>Eructavit cor meum.</i><br>My heart is inditing of a good matter : I speak of the things which I have made unto the King...</p><p><b>Psalm 46.</b> <i>Deus noster refugium.</i><br>God is our hope and strength : a very present help in trouble...</p>",
+            evening: "<h3>The Psalms</h3><p><b>Psalm 47.</b> <i>Omnes gentes, plaudite.</i><br>O clap your hands together, all ye people...</p>"
+        }
     }
 };
