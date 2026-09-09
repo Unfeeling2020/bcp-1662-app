d382c9f46e8723cf72ca403bddfadafcfc9ddae9
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 09:34:58 -0500

    Create bcp.js

---
+++ bcp.js
@@ -0,0 +1,11 @@
+const bcp = {
+    morningPrayer: {
+        title: "<h3>The Order for Morning Prayer</h3>",
+        exhortation: "<p><b>The Exhortation</b><br>Dearly beloved brethren, the Scripture moveth us in sundry places...</p>",
+        confession: "<p><b>The General Confession</b><br>Almighty and most merciful Father; We have erred, and strayed from thy ways like lost sheep...</p>",
+        absolution: "<p><b>The Absolution</b><br>Almighty God, the Father of our Lord Jesus Christ, who desireth not the death of a sinner...</p>",
+        lordsPrayer: "<p><b>The Lord's Prayer</b><br>Our Father, which art in heaven, Hallowed be thy Name...</p>",
+        preces: "<p><i>Priest.</i> O Lord, open thou our lips.<br><i>Answer.</i> And our mouth shall shew forth thy praise.</p>"
+    }
+    // We will add Evening Prayer, Holy Communion, and the Litany here later.
+};
