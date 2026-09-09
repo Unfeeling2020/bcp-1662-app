3cd666bca7674873f50dfd484c8a533c01d49123
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 10:42:58 -0500

    Create collects.js

---
+++ collects.js
@@ -0,0 +1,12 @@
+const bcpCollects = {
+    "Advent1": "<h3>The First Sunday in Advent</h3><p>Almighty God, give us grace that we may cast away the works of darkness, and put upon us the armour of light...</p>",
+    "ChristmasDay": "<h3>Christmas Day</h3><p>Almighty God, who hast given us thy only-begotten Son to take our nature upon him...</p>",
+    "AshWednesday": "<h3>Ash Wednesday</h3><p>Almighty and everlasting God, who hatest nothing that thou hast made, and dost forgive the sins of all them that are penitent...</p>",
+    "EasterDay": "<h3>Easter Day</h3><p>Almighty God, who through thine only-begotten Son Jesus Christ hast overcome death...</p>",
+    "AscensionDay": "<h3>Ascension Day</h3><p>Grant, we beseech thee, Almighty God, that like as we do believe thy only-begotten Son our Lord Jesus Christ to have ascended into the heavens...</p>",
+    "Pentecost": "<h3>Whit-Sunday</h3><p>God, who as at this time didst teach the hearts of thy faithful people, by the sending to them the light of thy Holy Spirit...</p>",
+    "TrinitySunday": "<h3>Trinity Sunday</h3><p>Almighty and everlasting God, who hast given unto us thy servants grace...</p>",
+    
+    // This is a placeholder for days you haven't typed in yet
+    "Default": "<h3>The Collect of the Day</h3><p><i>[Collect text will be added here]</i></p>"
+};
