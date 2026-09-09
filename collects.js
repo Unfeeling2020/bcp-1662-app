df0737429031e350c5650ac0d8c3d8d51cf91220
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 12:31:02 -0500

    Update collects.js

--- collects.js
+++ collects.js
@@ -1,12 +1,30 @@
 const bcpCollects = {
+    // --- ADVENT & CHRISTMAS ---
     "Advent1": "<h3>The First Sunday in Advent</h3><p>Almighty God, give us grace that we may cast away the works of darkness, and put upon us the armour of light...</p>",
+    "Advent2": "<h3>The Second Sunday in Advent</h3><p><i>[Add text here]</i></p>",
+    "Advent3": "<h3>The Third Sunday in Advent</h3><p><i>[Add text here]</i></p>",
+    "Advent4": "<h3>The Fourth Sunday in Advent</h3><p><i>[Add text here]</i></p>",
     "ChristmasDay": "<h3>Christmas Day</h3><p>Almighty God, who hast given us thy only-begotten Son to take our nature upon him...</p>",
-    "AshWednesday": "<h3>Ash Wednesday</h3><p>Almighty and everlasting God, who hatest nothing that thou hast made, and dost forgive the sins of all them that are penitent...</p>",
-    "EasterDay": "<h3>Easter Day</h3><p>Almighty God, who through thine only-begotten Son Jesus Christ hast overcome death...</p>",
-    "AscensionDay": "<h3>Ascension Day</h3><p>Grant, we beseech thee, Almighty God, that like as we do believe thy only-begotten Son our Lord Jesus Christ to have ascended into the heavens...</p>",
-    "Pentecost": "<h3>Whit-Sunday</h3><p>God, who as at this time didst teach the hearts of thy faithful people, by the sending to them the light of thy Holy Spirit...</p>",
-    "TrinitySunday": "<h3>Trinity Sunday</h3><p>Almighty and everlasting God, who hast given unto us thy servants grace...</p>",
+
+    // --- EPIPHANY ---
+    "Epiphany": "<h3>The Epiphany</h3><p>O God, who by the leading of a star didst manifest thy only-begotten Son...</p>",
+    "Epiphany1": "<h3>The First Sunday after the Epiphany</h3><p><i>[Add text here]</i></p>",
+    "Epiphany2": "<h3>The Second Sunday after the Epiphany</h3><p><i>[Add text here]</i></p>",
     
-    // This is a placeholder for days you haven't typed in yet
-    "Default": "<h3>The Collect of the Day</h3><p><i>[Collect text will be added here]</i></p>"
+    // --- LENT ---
+    "AshWednesday": "<h3>Ash Wednesday</h3><p>Almighty and everlasting God, who hatest nothing that thou hast made...</p>",
+    "Lent1": "<h3>The First Sunday in Lent</h3><p><i>[Add text here]</i></p>",
+    "Lent2": "<h3>The Second Sunday in Lent</h3><p><i>[Add text here]</i></p>",
+    
+    // --- EASTER ---
+    "Easter0": "<h3>Easter Day</h3><p>Almighty God, who through thine only-begotten Son Jesus Christ hast overcome death...</p>",
+    "Easter1": "<h3>The First Sunday after Easter</h3><p><i>[Add text here]</i></p>",
+    
+    // --- TRINITY SEASON ---
+    "TrinitySunday": "<h3>Trinity Sunday</h3><p>Almighty and everlasting God, who hast given unto us thy servants grace...</p>",
+    "Trinity1": "<h3>The First Sunday after Trinity</h3><p><i>[Add text here]</i></p>",
+    "Trinity2": "<h3>The Second Sunday after Trinity</h3><p><i>[Add text here]</i></p>",
+    // ... You can continue adding "Trinity3", "Trinity4", etc.
+
+    "Default": "<h3>The Collect of the Day</h3><p><i>[Collect text missing from collects.js]</i></p>"
 };
