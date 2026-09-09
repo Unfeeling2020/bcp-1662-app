bc87adcf4a4d4b516178b03e96ac7b4a6a1d32ae
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 10:24:40 -0500

    Create lectionary.js

---
+++ lectionary.js
@@ -0,0 +1,7 @@
+const fixedLectionary = {
+    "01-01": { "mpFirst": "Proper Lesson", "mpSecond": "Proper Lesson", "epFirst": "Proper Lesson", "epSecond": "Proper Lesson" },
+    "01-02": { "mpFirst": "Genesis 1", "mpSecond": "Matthew 1", "epFirst": "Genesis 2", "epSecond": "Romans 1" },
+    "01-03": { "mpFirst": "Genesis 3", "mpSecond": "Matthew 2", "epFirst": "Genesis 4", "epSecond": "Romans 2" },
+    // ... (We will use a small sample for testing, but imagine all 365 days are here)
+    "09-09": { "mpFirst": "Micah 6", "mpSecond": "Matthew 10", "epFirst": "Micah 7", "epSecond": "Romans 10" }
+};
