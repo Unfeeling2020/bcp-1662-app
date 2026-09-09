94d31422992db86d954a259bb2ca087c1a162b31
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 12:32:09 -0500

    Update bcp.js

--- bcp.js
+++ bcp.js
@@ -15,11 +15,21 @@ const bcp = {
         title: "<h3>The Litany</h3>",
         text: "<p>O God the Father, of heaven : have mercy upon us miserable sinners.</p>"
     },
-    psalter: {
-        day9: {
-            morning: "<h3>The Psalms</h3><p><b>Psalm 44.</b> <i>Deus, auribus.</i><br>We have heard with our ears, O God...</p>",
-            evening: "<h3>The Psalms</h3><p><b>Psalm 47.</b> <i>Omnes gentes.</i><br>O clap your hands together...</p>"
-        }
+ psalter: {
+            day1: {
+                morning: "<h3>The Psalms</h3><p><b>Psalm 1.</b> <i>Beatus vir, qui non abiit.</i><br>Blessed is the man that hath not walked in the counsel of the ungodly...</p><p><b>Psalm 2.</b> <i>Quare fremuerunt gentes?</i><br>Why do the heathen so furiously rage together...</p><p><b>Psalm 3.</b> <i>Domine, quid multiplicati?</i><br>Lord, how are they increased that trouble me...</p><p><b>Psalm 4.</b> <i>Cum invocarem.</i><br>Hear me when I call, O God of my righteousness...</p><p><b>Psalm 5.</b> <i>Verba mea auribus.</i><br>Ponder my words, O Lord : consider my meditation...</p>",
+                evening: "<h3>The Psalms</h3><p><b>Psalm 6.</b> <i>Domine, ne in furore.</i><br>O Lord, rebuke me not in thine indignation...</p><p><b>Psalm 7.</b> <i>Domine, Deus meus.</i><br>O Lord my God, in thee have I put my trust...</p><p><b>Psalm 8.</b> <i>Domine, Dominus noster.</i><br>O Lord our Governor, how excellent is thy Name in all the world...</p>"
+            },
+            day2: {
+                morning: "<h3>The Psalms</h3><p><b>Psalm 9.</b> <i>Confitebor tibi.</i><br>I will give thanks unto thee, O Lord, with my whole heart...</p><p><b>Psalm 10.</b> <i>Ut quid, Domine?</i><br>Why standest thou so far off, O Lord...</p><p><b>Psalm 11.</b> <i>In Domino confido.</i><br>In the Lord put I my trust : how say ye then to my soul...</p>",
+                evening: "<h3>The Psalms</h3><p><b>Psalm 12.</b> <i>Salvum me fac.</i><br>Help me, Lord, for there is not one godly man left...</p><p><b>Psalm 13.</b> <i>Usque quo, Domine?</i><br>How long wilt thou forget me, O Lord, for ever...</p><p><b>Psalm 14.</b> <i>Dixit insipiens.</i><br>The fool hath said in his heart : There is no God...</p>"
+            },
+            day9: {
+                morning: "<h3>The Psalms</h3><p><b>Psalm 44.</b> <i>Deus, auribus.</i><br>We have heard with our ears, O God...</p>",
+                evening: "<h3>The Psalms</h3><p><b>Psalm 47.</b> <i>Omnes gentes.</i><br>O clap your hands together...</p>"
+            }
+            // Add day3, day4, day5, etc. here!
+        },
     },
     // NEW LECTIONARY MAPPING
     lectionary: {
