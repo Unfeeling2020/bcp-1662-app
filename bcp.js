3beaeca6f305055301d2d90d972117fa9403edff
Unfeeling2020 <raitchi2@gmail.com>
2026-09-09 09:47:34 -0500

    Update bcp.js

--- bcp.js
+++ bcp.js
@@ -6,6 +6,9 @@ const bcp = {
         absolution: "<p><b>The Absolution</b><br>Almighty God, the Father of our Lord Jesus Christ, who desireth not the death of a sinner...</p>",
         lordsPrayer: "<p><b>The Lord's Prayer</b><br>Our Father, which art in heaven, Hallowed be thy Name...</p>",
         preces: "<p><i>Priest.</i> O Lord, open thou our lips.<br><i>Answer.</i> And our mouth shall shew forth thy praise.</p>"
+    },
+    litany: {
+        title: "<h3>The Litany</h3>",
+        text: "<p>O God the Father, of heaven : have mercy upon us miserable sinners.<br><i>O God the Father, of heaven : have mercy upon us miserable sinners.</i></p><p>O God the Son, Redeemer of the world : have mercy upon us miserable sinners...</p>"
     }
-    // We will add Evening Prayer, Holy Communion, and the Litany here later.
 };
